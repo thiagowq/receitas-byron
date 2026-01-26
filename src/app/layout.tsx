fb66694c905881ee8e358dbe92ea954c8f40a630
Thiago José da Cruz <tj3137488@gmail.com>
2026-01-26 20:19:51 -0300

    feat/estrutura do footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter ({
   variable: "--font-inter",
@@ -25,6 +26,9 @@ export default function RootLayout({
       >
         <Header />
         {children}
+
+        <Footer />
+
       </body>
     </html>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return(
+
+        <footer>
+            <div>
+                <p>© 2025 Receitas Deliciosas. Todos os direitos reservados</p>
+            </div>
+        </footer>
+    )
+}
