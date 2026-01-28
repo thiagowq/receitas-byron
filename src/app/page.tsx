e0f96af64be8fedefdcb374f9ad13d8c9397c656
Thiago José da Cruz <tj3137488@gmail.com>
2026-01-28 11:15:42 -0300

    feat: adiciona estilos da secao hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex-grow">
-      <div className="container mx-auto">
-        {/* Seção Hero */}
-        
-        <section> 
-          
-          <h1>Receitas Deliciosas</h1>
-          <p>Descubra receitas simples e deliciosas para todas as ocasiões</p>
 
-          <Link href="/receitas">
-            Ver todas as receitas
-          </Link>
-          
+        {/* Seção Hero */}  
+        <section className="bg-orange-50 py-10">
+          <div className="container mx-auto flex flex-col gap-4 items-center">
+            <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
+            <p className="text-xl">Descubra receitas simples e deliciosas para todas as ocasiões</p>
+
+            <Link className="bg-orange-500 text-white font-bold rounded-lg px-3 py-2" href="/receitas">
+              Ver todas as receitas
+            </Link>
+          </div> 
         </section>
-      </div>
     </main>
   );
 }
