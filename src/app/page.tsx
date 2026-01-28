3468fd2d132c4027858b07e62c44cf6229aa64f3
Thiago José da Cruz <tj3137488@gmail.com>
2026-01-28 11:16:58 -0300

    style: hover no botao de receitas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <main className="flex-grow">
 
         {/* Seção Hero */}  
-        <section className="bg-orange-50 py-10">
-          <div className="container mx-auto flex flex-col gap-4 items-center">
+        <section className="bg-orange-50 py-12">
+          <div className="container mx-auto flex flex-col gap-6 items-center">
             <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
             <p className="text-xl">Descubra receitas simples e deliciosas para todas as ocasiões</p>
 
-            <Link className="bg-orange-500 text-white font-bold rounded-lg px-3 py-2" href="/receitas">
+            <Link className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas">
               Ver todas as receitas
             </Link>
           </div> 
