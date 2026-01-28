049ca62d0115e7ae12445d2749ef176611ce752e
Thiago José da Cruz <tj3137488@gmail.com>
2026-01-28 11:08:55 -0300

    feat: estrutura da seção hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1> Tela inicial </h1>
+      <div className="container mx-auto">
+        {/* Seção Hero */}
+        
+        <section> 
+          
+          <h1>Receitas Deliciosas</h1>
+          <p>Descubra receitas simples e deliciosas para todas as ocasiões</p>
+
+          <Link href="/receitas">
+            Ver todas as receitas
+          </Link>
+          
+        </section>
+      </div>
     </main>
   );
 }
