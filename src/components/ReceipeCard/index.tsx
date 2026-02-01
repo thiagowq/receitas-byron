7ae6e55ac86889acce8be4f0fc595a8a5946fba9
Thiago José da Cruz <tj3137488@gmail.com>
2026-02-01 19:53:32 -0300

    feat: previne comportamento de navegacao nos botões de editar e deletar

--- src/components/ReceipeCard/index.tsx
+++ src/components/ReceipeCard/index.tsx
@@ -1,4 +1,5 @@
 import { Recipe } from "@/lib/data";
+import { Edit, Trash2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,21 +8,47 @@ interface ReceipeCardProps{
 }
 
 export default function RecipeCard({ recipe }: ReceipeCardProps) {
+    const handleEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+    }
+
+    const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+    }
+
     return (
         <Link href={`/receitas/${recipe.id}`}>
         <div className="border border-slate-200 rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow">
+            {/* Imagem */}
             <div className="relative h-48 w-full">
-                <Image 
-                    src={recipe.image}
-                    fill
-                    alt={recipe.title}
-                    className="object-cover"
-                    />
+                <Image src={recipe.image} fill alt={recipe.title} className="object-cover"/>
             </div>
 
-            <div className="flex flex-col p-4 gap-2">
-                <h3 className="text-lg font-bold">{recipe.title}</h3>
-                <p className="text-gray-600">{recipe.description}</p>
+            <div className="flex flex-col p-4 gap-6">
+                {/* Título e descrição */}
+                <div className="space-y-2">
+                    <h3 className="text-lg font-bold hover:text-orange-500 transition-colors">{recipe.title}</h3>
+                    <p className="text-gray-600">{recipe.description}</p>
+                </div>
+
+                {/* Categoria e ações */}
+                <div className="flex justify-between items-center w-full">
+                    <span className="text-sm text-gray-500 rounded bg-gray-100 px-2 py-1">
+                        {recipe.category}
+                    </span>
+
+                    <div className="flex gap-2">
+                        {/* Botão de editar */}
+                        <button type="button" onClick={(e) => handleEdit(e)} className="p-2 border-gray-400 rounded hover:bg-gray-600 transition-colors cursor-pointer">
+                            <Edit size={16}/>
+                        </button>
+                        
+                        {/* Botão de remover */}
+                        <button type="button" onClick={(e) => handleDelete(e)} className="p-2 border-gray-400 rounded hover:bg-gray-600 transition-colors cursor-pointer">
+                            <Trash2 size={16}/>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </Link>
